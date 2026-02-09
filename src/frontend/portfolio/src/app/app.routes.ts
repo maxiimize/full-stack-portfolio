4c391dca887a5b40574e3aa7e10dada1c663d2fe
maxiimize <max.b@hotmail.se>
2026-02-09 14:15:32 +0100

    feat(frontend): add project list page

--- src/frontend/portfolio/src/app/app.routes.ts
+++ src/frontend/portfolio/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./components/project-list/project-list.component').then(
+        (m) => m.ProjectListComponent
+      ),
+  },
+];
